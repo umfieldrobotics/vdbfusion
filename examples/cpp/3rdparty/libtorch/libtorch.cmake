436c57799d5d2487ce0e98b3883ba7f5ff8f8be6
Anja Sheppard <amstrudy@gmail.com>
2024-03-20 10:20:51 -0400

    cmake changes to bring in torch and open3d

---
+++ examples/cpp/3rdparty/libtorch/libtorch.cmake
@@ -0,0 +1,3 @@
+# Written by Anja Sheppard, February 2024
+
+find_package(Torch REQUIRED HINTS "/home/anjashep-frog-lab/libtorch/")
---
+++ examples/cpp/3rdparty/open3d/open3d.cmake
@@ -0,0 +1,3 @@
+# Written by Anja Sheppard, February 2024
+
+find_package(Open3D REQUIRED HINTS "/home/anjashep-frog-lab/open3d-0.18.0/")
