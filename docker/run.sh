b4d622dff56bfcf374be944c3812182b1c3b221d
yuzhen chen <yuzhench@umich.edu>
2024-06-20 18:24:20 -0400

    update the dockerfile and run.sh file

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,9 @@
 
 docker run -it \
     -e HOME \
-    -v $HOME:/home/anjashep-frog-lab \
-    -v /usr/local/lib/cmake:/usr/local/lib/cmake \
+    -v /home/yuzhen/Desktop/semanticVDB:/home/yuzhen/Desktop/semanticVDB \
+    -v /usr/local/lib/cmake/OpenVDB:/usr/local/lib/cmake/OpenVDB \
+    -v /usr/local/lib/cmake/VDBFusion:/usr/local/lib/cmake/VDBFusion \
     --name vdbfusion_docker \
     --gpus all \
     vdbfusion_docker # TODO: change this image name, container name and home directory accordingly
